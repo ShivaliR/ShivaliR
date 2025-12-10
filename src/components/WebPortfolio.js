312412115feb1db8aed7c6fe8c85688c59131ba8
Shivali Ranka <shivaliranka12@gmail.com>
2025-12-10 21:56:47 +0530

    updating

--- src/components/WebPortfolio.js
+++ src/components/WebPortfolio.js
@@ -4,12 +4,12 @@ import './Webportfolio.css';
 function WebPortfolio() {
   return (
     <section id="web-portfolio-section" className="web-portfolio-section-class">
-      <h2 id="portfolio-heading" className="portfolio-heading">
+      {/* <h2 id="portfolio-heading" className="portfolio-heading">
       Web Development Projects
-      </h2>
+      </h2> */}
       {/* Web Development Projects Section */}
       <div id="web-projects" className="portfolio-category">
-        On this page, I'll list all the projects and apps I've made.
+        <h2>On this page, I'll list all the projects and apps I've made.</h2>
       </div>
 
     </section>
