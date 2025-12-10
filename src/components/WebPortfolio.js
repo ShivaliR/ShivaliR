92d573b16286ea42c4f8b2aea879a790f42165f1
Shivali Ranka <shivaliranka12@gmail.com>
2025-12-10 21:53:13 +0530

    updating

--- src/components/WebPortfolio.js
+++ src/components/WebPortfolio.js
@@ -9,40 +9,7 @@ function WebPortfolio() {
       </h2>
       {/* Web Development Projects Section */}
       <div id="web-projects" className="portfolio-category">
-        <div className="projects-list">
-          <div className="project-card">
-            <img
-              src="/images/client-1.jpg" 
-              alt="Project 1"
-              className="project-image"
-            />
-            <h4 className="project-title">Project 1</h4>
-          </div>
-          <div className="project-card">
-            <img
-              src="/images/image.png"
-              alt="Project 2"
-              className="project-image"
-            />
-            <h4 className="project-title">Project 2</h4>
-          </div>
-           <div className="project-card">
-            <img
-              src="/images/client-3.jpg" 
-              alt="Project 3"
-              className="project-image"
-            />
-            <h4 className="project-title">Project 3</h4>
-          </div>
-           <div className="project-card">
-            <img
-              src="/images/client-4.jpg" 
-              alt="Project 4"
-              className="project-image"
-            />
-            <h4 className="project-title">Project 4</h4>
-          </div>
-        </div>
+        On this page, I'll list all the projects and apps I've made.
       </div>
 
     </section>
